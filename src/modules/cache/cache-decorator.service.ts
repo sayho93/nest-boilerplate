41d021ab0dfa07f22fd361409260869c5ef28ba3
sayho <fishcreek@naver.com>
2024-05-14 19:33:28 +0900

    fix: minor

--- src/modules/cache/cache-decorator.service.ts
+++ src/modules/cache/cache-decorator.service.ts
@@ -2,22 +2,29 @@ import { Cache, CacheOptions } from '@nestjs/cache-manager';
 import { CACHE_DECORATOR } from './cache.constant';
 import { Aspect } from '../aop/aop.decorator';
 import { LazyDecorator, WrapParams } from '../aop/aop.interface';
+import { LoggerService } from '../logger/logger.service';
 
 @Aspect(CACHE_DECORATOR)
 export class CacheDecoratorService implements LazyDecorator<any, CacheOptions> {
-  constructor(private readonly cache: Cache) {}
+  constructor(
+    private readonly cache: Cache,
+    private readonly loggerService: LoggerService,
+  ) {}
 
-  wrap(params: WrapParams<any, CacheOptions>) {
+  public wrap(params: WrapParams<any, CacheOptions>) {
     const key = `${params.instance.constructor.name}.${params.methodName}`;
 
     return async (...args: any[]) => {
-      console.log(args);
-
       const cachedValue = await this.cache.get(key);
-      if (cachedValue) return cachedValue;
+      if (cachedValue) {
+        this.loggerService.verbose(this.wrap.name, { args, key }, 'cache hit');
+        return cachedValue;
+      }
 
       const methodResult = await params.method(...args);
       await this.cache.set(key, methodResult);
+
+      this.loggerService.verbose(this.wrap.name, { args, key }, 'cache miss');
 
       return methodResult;
     };
